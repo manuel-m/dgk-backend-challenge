e205c0a69bd57f404e1aa6a455d125563ae2e525
mmi <efficiel.technologie@gmail.com>
2022-06-30 18:38:43 +0200

    e2e, 1st post

--- tests/src/e2e.tests/e2e.tests.index.js
+++ tests/src/e2e.tests/e2e.tests.index.js
@@ -1,1 +1,17 @@
-console.log("e2e tests!!!");
+import axios from "axios";
+
+import { conf } from "../../../devops/conf.mjs";
+
+_main();
+
+async function _main() {
+  const response = await axios.post(_url("users"));
+
+  const { data, status } = response;
+
+  console.log({ data, status });
+}
+
+function _url(uri) {
+  return "http://localhost:" + conf.mservicesMap.nginx.port + "/" + uri;
+}
